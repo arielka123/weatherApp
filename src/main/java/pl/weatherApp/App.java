3763546b8a3cdcad41cc3a48b4a2f2c51f46592f
Arleta Madej <arleta.madej@gmail.com>
2023-12-15 12:01:52 +0100

    Revert "added borderPane.fxml"
    This reverts commit 3f3d113f674f9da0da06867ebd796a4db6f87a55.

--- src/main/java/pl/weatherApp/App.java
+++ src/main/java/pl/weatherApp/App.java
@@ -1,20 +1,13 @@
 package pl.weatherApp;
-import javafx.application.Application;
-import javafx.stage.Stage;
 
 /**
  * Hello world!
  *
  */
-public class App extends Application {
+public class App 
+{
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        launch(args);
-    }
-
-    @Override
-    public void start(Stage stage) throws Exception {
-
     }
 }
--- src/main/java/pl/weatherApp/Main.java
+++ src/main/java/pl/weatherApp/Main.java
@@ -1,7 +1,2 @@
-package pl.weatherApp;
-
-public class Main {
-    public static void main(String[] args) {
-        App.main(args);
-    }
+package pl.weatherApp;public class Main {
 }
