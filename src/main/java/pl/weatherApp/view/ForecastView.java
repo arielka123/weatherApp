1d9dfd2ddb9a4bca85d5add5aa8c8f49ee344f30
Arleta Madej <arleta.madej@gmail.com>
2024-02-14 20:26:59 +0100

    some changes for forecast view

--- src/main/java/pl/weatherApp/view/ForecastView.java
+++ src/main/java/pl/weatherApp/view/ForecastView.java
@@ -26,14 +26,8 @@ public class ForecastView {
 
     public void create() {
         for (int i = 0; i < days; i++) {
-            TilePane tilePaneInner = new TilePane();
-
-            TilePane.setMargin(tilePaneInner, new Insets(3));
-
-            tilePaneInner.setPrefRows(6);
-            tilePaneInner.setPrefColumns(1);
-            tilePaneInner.setAlignment(Pos.TOP_CENTER);
-            tilePaneInner.setStyle("-fx-background-color:rgb(237,237,237); -fx-background-radius:30;");
+            TilePane tilePaneInner = getTilePaneInner();
+            tilePaneInner.setPadding(new Insets(10,10,10,10));
 
             Label labelDate = new Label();
             Label labelDayWeek = new Label();
@@ -43,12 +37,7 @@ public class ForecastView {
             Label labelPrecipitation = new Label();
             Label labelWindSpeed1 = new Label();
 
-            labelFeelsLike.setPadding(new Insets(2));
-            countryCode.setTextFill(Color.GRAY);
-            labelDayWeek.setStyle("-fx-font-weight: bold");
-            labelDate.setFont(Font.font(10));
-            labelDate.setTextFill(Color.GRAY);
-            labelInfo.setTextFill(Color.valueOf("#2554c7"));
+            setStylesForLabels(labelFeelsLike, labelDayWeek, labelDate, labelInfo, labelTempMin);
 
             int windValue = forecastCollection.getOneOfForecastList(i).getWindDirection();
             String directions = Directions.findDirectionName(windValue);
@@ -56,20 +45,51 @@ public class ForecastView {
             labelDayWeek.setText(forecastCollection.getOneOfForecastList(i).getDayOfWeek());
             labelDate.setText(forecastCollection.getOneOfForecastList(i).getDateStr());
             labelInfo.setText(forecastCollection.getOneOfForecastList(i).getWeather_code());
-            labelTempMin.setText(Utils.getResourceBundle().getString("weather.temperature") +": "+ forecastCollection.getOneOfForecastList(i).getTempMin() + Units.temperature);
+            labelTempMin.setText(forecastCollection.getOneOfForecastList(i).getTempMin() + Units.temperature);
             labelFeelsLike.setText(Utils.getResourceBundle().getString("weather.feelsLike") +": "+ forecastCollection.getOneOfForecastList(i).getFeels_likeMin() + Units.temperature);
             labelPrecipitation.setText(Utils.getResourceBundle().getString("weather.precipitation") +": "+ forecastCollection.getOneOfForecastList(i).getPrecipitation() + Units.precipitation);
-            labelWindSpeed1.setText(Utils.getResourceBundle().getString("weather.wind") + ": "+ directions + forecastCollection.getOneOfForecastList(i).getWindSpeed() + Units.wind);
+            labelWindSpeed1.setText(directions +" "+ forecastCollection.getOneOfForecastList(i).getWindSpeed() + Units.wind);
             countryCode.setText(forecastCollection.getOneOfForecastList(i).getCountryCode());
 
             tilePaneInner.getChildren().add(labelDayWeek);
             tilePaneInner.getChildren().add(labelDate);
-            tilePaneInner.getChildren().add(labelInfo);
             tilePaneInner.getChildren().add(labelTempMin);
+            tilePaneInner.getChildren().add(labelInfo);
             tilePaneInner.getChildren().add(labelFeelsLike);
             tilePaneInner.getChildren().add(labelPrecipitation);
             tilePaneInner.getChildren().add(labelWindSpeed1);
             tilePaneId.getChildren().add(tilePaneInner);
         }
+    }
+
+    private void setStylesForLabels(Label labelFeelsLike, Label labelDayWeek, Label labelDate, Label labelInfo, Label labelTempMin) {
+
+        labelFeelsLike.setPadding(new Insets(2));
+
+        countryCode.setTextFill(Color.GRAY);
+
+        labelDayWeek.setStyle("-fx-font-weight: bold");
+
+        labelDate.setFont(Font.font(10));
+        labelDate.setTextFill(Color.valueOf("a3a2a2"));
+
+        labelInfo.setTextFill(Color.valueOf("a3a2a2"));
+        labelInfo.setStyle("-fx-font-weight: bold");
+
+        labelTempMin.setFont(Font.font(14));
+        labelTempMin.setStyle("-fx-font-weight: bold");
+        labelTempMin.setTextFill(Color.valueOf("a3a2a2"));
+    }
+
+    private static TilePane getTilePaneInner() {
+        TilePane tilePaneInner = new TilePane();
+
+        TilePane.setMargin(tilePaneInner, new Insets(3));
+
+        tilePaneInner.setPrefRows(6);
+        tilePaneInner.setPrefColumns(1);
+        tilePaneInner.setAlignment(Pos.TOP_CENTER);
+        tilePaneInner.setStyle("-fx-background-color:rgb(237,237,237); -fx-background-radius:30;");
+        return tilePaneInner;
     }
 }
